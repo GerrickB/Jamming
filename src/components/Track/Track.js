163ffdc888e7fcc177cf618e93106feea05fe6a9
GerrickB <27402037+GerrickB@users.noreply.github.com>
2023-05-10 14:34:27 -0700

    Add track mock data for testing

---
+++ src/components/Track/Track.js
@@ -0,0 +1,20 @@
+export const tracks = [
+  {
+    id: 0,
+    name: '',
+    artist: '',
+    album: ''
+  },
+  {
+    id: 1,
+    name: '',
+    artist: '',
+    album: ''
+  },
+  {
+    id: 2,
+    name: '',
+    artist: '',
+    album: ''
+  }
+];
