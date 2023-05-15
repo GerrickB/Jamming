6452c4b70e259fb01477c048e17cfe0558c4651a
GerrickB <27402037+GerrickB@users.noreply.github.com>
2023-05-15 11:07:27 -0700

    Update Track listing component

--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -1,23 +1,35 @@
 import React from "react";
 import "./Tracklist.css"
+import { getImageUrl } from "../../utils/utils";
 import { useState } from "react";
 import { tracks } from "../Track/Track";
 
 export default function Tracklist() {
   const [trackResults, setTrackResults] = useState([]);
-
+  const listItems = tracks.map((track) => (
+    <li key={track.id}>
+      <img
+        src={getImageUrl(track)} 
+        alt={track.name}
+      />
+      <p>
+        {track.name}
+        <span>{<br/>}</span>
+        {track.artist}
+      </p>
+      <button>PLUS</button>
+    </li>
+  ))
   return (
     <>
       <h2>Track List</h2>
-      <ul>
-        {tracks.map((track) => (
-          <li key={track.id}>
-            {track.name} <button>PLUS</button>
-            <hr/>
-          </li>
-        ))}
-      </ul>
+      <ul>{listItems}</ul>
     </>
   )
-
 }
+
+/*
+TO DO:
+  make ul display: grid
+  test play list beside track list
+*/
