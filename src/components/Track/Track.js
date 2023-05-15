27aae260407f859c74cb94e5e75ef69c14e2f371
GerrickB <27402037+GerrickB@users.noreply.github.com>
2023-05-14 22:31:16 -0700

    Add imageId property to track object

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -3,18 +3,21 @@ export const tracks = [
     id: 0,
     name: 'Hold On Tight',
     artist: 'aespa',
-    album: 'Hold On Tight'
+    album: 'Hold On Tight',
+    imageId: '3pI8wX0'
   },
   {
     id: 1,
     name: 'Karma',
     artist: 'Taylor Swift',
-    album: 'Midnights'
+    album: 'Midnights',
+    imageId: 'VEEta9i'
   },
   {
     id: 2,
     name: 'Summertime Sadness',
     artist: 'Lana Del Rey',
-    album: 'Born To Die'
+    album: 'Born To Die',
+    imageId: 'QRaLfRj'
   }
 ];
