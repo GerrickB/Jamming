1d1e02c3825a7e1d472d89457298e238f17ef984
GerrickB <27402037+GerrickB@users.noreply.github.com>
2023-05-18 17:32:54 -0700

    Implement useState for adding and deleting playlist

--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -3,10 +3,35 @@ import "./Tracklist.css"
 import { getImageUrl } from "../../utils/utils";
 import { useState } from "react";
 import { tracks } from "../Track/Track";
+import Playlist from "../Playlist/Playlist";
 
 export default function Tracklist() {
-  const [trackResults, setTrackResults] = useState([]);
-  const listItems = tracks.map((track) => (
+  
+  const [playlist, setPlaylist] = useState([]);
+  const [playlistName, setPlaylistName] = useState('');
+
+  /*
+  function handleAddClick() {
+    playlist.map(song => (
+      setPlaylist([
+        ...playlist,
+        { id: song.id, name: song.name }
+      ])
+    ))
+  }
+  */
+  
+  function handleDeleteClick() {
+    playlist.map(song => (
+      setPlaylist(
+        playlist.filter(p =>
+          p.id !== song.id
+        )
+      )
+    ))
+  }
+
+  const listAllTracks = tracks.map((track) => (
     <li key={track.id}>
       <img
         src={getImageUrl(track)} 
@@ -17,25 +42,30 @@ export default function Tracklist() {
         <span>{<br/>}</span>
         {track.artist}
       </p>
-      <button>PLUS</button>
+      <button onClick={() => {
+        setPlaylist([
+          ...playlist,
+          { id: track.id, name: track.name } 
+          // export object to Playlist component then convert to array
+          // props.playlist.map
+        ]);
+      }}>
+        +
+      </button>
     </li>
   ))
+
   return (
     <div class="main-grid">
       <div class='track-list'>
         <h2>Track List</h2>
-        <ul>{listItems}</ul>
+        <ul>{listAllTracks}</ul>
       </div>
       <div className="playlist">
-        <h2>playlist</h2>
+        <h2>My playlist</h2>
+        <Playlist newPlaylist={playlist} delete={handleDeleteClick} />
       </div>
 
     </div>
   )
 }
-
-/*
-TO DO:
-  make ul display: grid
-  test play list beside track list
-*/
